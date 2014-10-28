38ecb290316f916f17b3c9d5451bc332ded8136f
Kouji Matsui <k@kekyo.net>
2014-10-29 00:11:32 +0900

    Version 2 (Basic reflection)

--- CenterCLR.Demo1/Program.cs
+++ CenterCLR.Demo1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -9,9 +11,16 @@ namespace CenterCLR.Demo1
 {
 	public static class MessageManager
 	{
-		public static string GetMessage(string message)
+		public static string GetMessage(Type type, string fieldName)
 		{
-			return message;
+			var fieldInfo = type.GetField(fieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly);
+			var key = string.Format("{0}.{1}.{2}", fieldInfo.DeclaringType.Namespace, fieldInfo.DeclaringType.Name, fieldInfo.Name);
+				
+			var value = (string)fieldInfo.GetValue(null);
+
+			// TODO:キーを使って何かする
+
+			return value;
 		}
 	}
 
@@ -22,8 +31,8 @@ namespace CenterCLR.Demo1
 
 		static void Main(string[] args)
 		{
-			var title = MessageManager.GetMessage(TitleMessage);
-			var description = MessageManager.GetMessage(DescriptionMessage);
+			var title = MessageManager.GetMessage(typeof(Program), "TitleMessage");
+			var description = MessageManager.GetMessage(typeof(Program), "DescriptionMessage");
 
 			MessageBox.Show(description, title, MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
 		}
