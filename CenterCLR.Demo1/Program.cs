35a0ae7480b3c99bb08fe98f350ed64cb190ddd3
Kouji Matsui <k@kekyo.net>
2014-10-28 23:56:10 +0900

    First version.

--- CenterCLR.Demo1/Program.cs
+++ CenterCLR.Demo1/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CenterCLR.Demo1
 {
+	public static class MessageManager
+	{
+		public static string GetMessage(string message)
+		{
+			return message;
+		}
+	}
+
 	class Program
 	{
+		private static readonly string TitleMessage = "○×商事CRMシステム";
+		private static readonly string DescriptionMessage = "宜しいですか？";
+
 		static void Main(string[] args)
 		{
+			var title = MessageManager.GetMessage(TitleMessage);
+			var description = MessageManager.GetMessage(DescriptionMessage);
+
+			MessageBox.Show(description, title, MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
 		}
 	}
 }
