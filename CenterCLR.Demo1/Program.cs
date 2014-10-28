4e09e08687161dd07d2c3c8c8a85e346e91f3b25
Kouji Matsui <k@kekyo.net>
2014-10-29 00:24:13 +0900

    Version 3 (Expression Tree)

--- CenterCLR.Demo1/Program.cs
+++ CenterCLR.Demo1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -11,9 +12,18 @@ namespace CenterCLR.Demo1
 {
 	public static class MessageManager
 	{
-		public static string GetMessage(Type type, string fieldName)
+		public static string GetMessage(Expression<Func<string>> field)
 		{
-			var fieldInfo = type.GetField(fieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.DeclaredOnly);
+			// 戻り値がstringとなる式である事は、タイプセーフ性で保証されている。
+			// フィールドを示す式である事だけを確認する。
+
+			var memberExpression = field.Body as MemberExpression;
+			Debug.Assert(memberExpression != null);
+
+			var fieldInfo = memberExpression.Member as FieldInfo;
+			Debug.Assert(fieldInfo != null);
+			Debug.Assert(fieldInfo.IsStatic == true);
+
 			var key = string.Format("{0}.{1}.{2}", fieldInfo.DeclaringType.Namespace, fieldInfo.DeclaringType.Name, fieldInfo.Name);
 				
 			var value = (string)fieldInfo.GetValue(null);
@@ -31,8 +41,8 @@ namespace CenterCLR.Demo1
 
 		static void Main(string[] args)
 		{
-			var title = MessageManager.GetMessage(typeof(Program), "TitleMessage");
-			var description = MessageManager.GetMessage(typeof(Program), "DescriptionMessage");
+			var title = MessageManager.GetMessage(() => TitleMessage);
+			var description = MessageManager.GetMessage(() => DescriptionMessage);
 
 			MessageBox.Show(description, title, MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
 		}
